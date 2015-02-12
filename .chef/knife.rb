ea534b09dd98185ce6867a115479c8d918f40d98
Venkateswara Reddy Daram <venkateshdaram434@gmail.com>
2015-02-12 20:40:34 +0530

    adding new

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
 node_name                'venkat'
-client_key               '/root/chef-repo/.chef/venkat.pem'
+client_key               '/home/venkat/chef-cookbooks/.chef/venkat.pem'
 validation_client_name   'chef-validator'
-validation_key           '/etc/chef-server/chef-validator.pem'
+validation_key           '/home/venkat/chef-cookbooks/chef-validator.pem'
 chef_server_url          'https://chef-server.venkat.com:443'
 syntax_check_cache_path  '/root/chef-repo/.chef/syntax_check_cache'
 cookbook_path		 '/home/venkat/chef-cookbooks/cookbooks'
